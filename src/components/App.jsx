ca21d44cd846f3d3564ba2daff38ca5a3f5371aa
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2023-02-17 00:20:14 +0200

    hw-1-start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import { Profile } from "./Profile"
+import user from "../components/user.json"
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+return <div>
+  <Profile username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+</div>
+}
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
